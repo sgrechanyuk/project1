2690ad3abbb985deb5ce91ad16c1e1ffed701dfd
Kristina Shandrenko <kristinashandrenko@MacBook-Pro-Kristina.local>
2015-11-13 21:52:48 +0200

    test gradle project

---
+++ src/test/java/TestMe.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by kristinashandrenko on 13.11.15.
+ */
+public class TestMe {
+
+    @Test
+    public void testMe(){
+        assertTrue(2==2);
+    }
+
+}
